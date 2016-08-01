19b62bedbf5364c36af0b4f4bdcdb9f68286e638
AfroMonkey <moisalejandro@gmail.com>
2016-08-01 11:21:16 -0500

    [INIT]

---
+++ README.md
@@ -0,0 +1,3 @@
+Change Partner City Position
+============================
+TODO [IMP]
---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Change Partner City Position',
+    'version': '1.0',
+    'author': 'Humanytek',
+    'website': 'http://humanytek.com',
+    'depends': [],  # TODO [IMP]
+    'data': [
+        'views/change_partner_city_position.xml',
+    ]
+}
